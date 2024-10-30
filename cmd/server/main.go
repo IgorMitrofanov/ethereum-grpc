a917c84c3b16b74b4f54c977687d3c265123788e
IgorMitrofanov <i.v.mitrofanov@ya.ru>
2024-10-30 17:33:25 +0300

    client-server add

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,45 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"net"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
+	"google.golang.org/grpc"
+
+	"ethereum-grpc/internal/proto"
 )
+
+type server struct {
+	proto.UnimplementedEthereumServiceServer
+	client *ethclient.Client
+}
+
+func (s *server) GetAccount(ctx context.Context, req *proto.GetAccountRequest) (*proto.GetAccountResponse, error) {
+	accountAddress := common.HexToAddress(req.EthereumAddress)
+
+	balance, err := s.client.BalanceAt(ctx, accountAddress, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	nonce, err := s.client.NonceAt(ctx, accountAddress, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	gasTokenBalance := balance.String()
+
+	response := &proto.GetAccountResponse{
+		GastokenBalance: gasTokenBalance,
+		WalletNonce:     nonce,
+	}
+
+	return response, nil
+}
 
 func main() {
 	client, err := ethclient.Dial("https://mainnet.infura.io")
@@ -13,6 +47,16 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println("we have a connection")
-	_ = client // we'll use this in the upcoming sections
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatal(err)
+	}
+	grpcServer := grpc.NewServer()
+
+	proto.RegisterEthereumServiceServer(grpcServer, &server{client: client})
+
+	fmt.Println("gRPC server listening on port 50051")
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatal(err)
+	}
 }
